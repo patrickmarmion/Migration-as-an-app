f18a211d21ff77a5cda91d2330a96c8584a4a7e7
Patrick Marmion <patrick.marmion@pandadoc.com>
2023-10-09 18:59:26 +0100

    buf debug

--- Authentication/verifyWebhookSignature.js
+++ Authentication/verifyWebhookSignature.js
@@ -1,13 +1,13 @@
 const crypto = require('crypto');
 require('dotenv').config();
 
-const verifyWebhookSignature = async (req, res, buf) => {
+const verifyWebhookSignature = async (req, res) => {
     const signature = req.query.signature;
     console.log(process.env.SHARED_KEY);
     console.log("signature: " + signature);
     if (signature && req.method === "POST") {
-        const hmac = crypto.createHmac("sha256", process.env.SHARED_KEY);
-        hmac.update(buf);
+        const buf = Buffer.from(JSON.stringify(req.body));
+        const hmac = crypto.createHmac("sha256", process.env.SHARED_KEY).update(buf);
         const digest = hmac.digest("hex");
         console.log("This is the signature: " + digest);
 
